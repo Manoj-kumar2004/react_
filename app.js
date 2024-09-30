635c7b2a8ab5166cb67b13401c53f08dafcdf5f3
Manoj <manojkumar2k4.g@gmail.com>
2024-09-30 18:54:42 +0530

    Included Tailwind.css

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 const parent = React.createElement('div',{id : "parent"},
     [React.createElement("div",{id:"child"},
 
-        [React.createElement("h1",{},"HELOO MAN"),React.createElement("h1",{},"HELOO MAN")]
+        [React.createElement("h1",{},"Kratos"),React.createElement("h1",{},"Kratos")]
     ),React.createElement("div",{id: "child2"},
 
-        [React.createElement("h1",{},"HELOO MAN"),React.createElement("h1",{},"HELOO MAN")]
+        [React.createElement("h2",{},"Kratos"),React.createElement("h3",{},"Kratos")]
     )]
 
 
